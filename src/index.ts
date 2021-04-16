d17667f12bdf75f0da950819f93ff908e1cd7d0b
KristalBueno1 <kristalbueno1805@gmail.com>
2021-04-15 16:52:18 -1200

    insertando usuarios

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 
-import "./firebase/config";
+import db "./firebase/config";
 
+const usuario = {
+    nombre: "Fernando",
+    activo: true,
+    fechaNaci: 0
+}
 
+// insert into usuarios...
+
+// usuarios_ref
+    //     .add(usuario)
+
+    //         /db.collection('usuarios')
+    // .add(usuario)
+    // .then( docRef => {
+    //    console.log( docRef )
+    // })
+    // .catch(e => console.log('error', e))
