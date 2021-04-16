a27ccd6a4d11a56a61434bd8193b8a2271c547ca
KristalBueno1 <kristalbueno1805@gmail.com>
2021-04-15 17:05:22 -1200

    actualizando data en firestore

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,39 @@
 import db "./firebase/config";
 
 const usuario = {
-    nombre: "Fernando",
+    nombre: "Kristal",
     activo: true,
     fechaNaci: 0
 }
 
+const usuariosRef = db.collection("usuarios");
+
 // insert into usuarios...
-
 // usuarios_ref
-    //     .add(usuario)
+//     .add(usuario)
 
-    //         /db.collection('usuarios')
-    // .add(usuario)
-    // .then( docRef => {
-    //    console.log( docRef )
-    // })
-    // .catch(e => console.log('error', e))
+//         /db.collection('usuarios')
+// .add(usuario)
+// .then( docRef => {
+//    console.log( docRef )
+// })
+// .catch(e => console.log('error', e))
+
+// update usuarios set activo = false where...
+// usuarios_ref.doc("
+// yFRoW26ja8zUggGYPAGJ
+// ")
+//         .update({
+//             activo: false
+//         })
+
+// destructivo
+    // usuarios_ref.doc("
+    // yFRoW26ja8zUggGYPAGJ
+    // ")
+    //         .set({
+    //             nombre: mariel,
+    //             activo: true,
+    //             fechaNaci: 20,
+    //             edad: 10
+    //         })
